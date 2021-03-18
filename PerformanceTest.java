12fbb74301b2c14e1c601ea41354de73c09d4c6e
StefanyTodoroff <72222524+StefanyTodoroff@users.noreply.github.com>
2021-03-18 18:25:07 +0200

    Create PerformanceTest.java

---
+++ PerformanceTest.java
@@ -0,0 +1,20 @@
+package generics;
+import java.util.LinkedList;
+
+public class PerformanceTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		LinkedList<Integer> numbers = new LinkedList<>();
+		for( int i = 0; i < 5_000_000; i++) {
+			numbers.add(i);
+		}
+		int a = 0;
+		for(Integer number: numbers) {
+			a = number;
+		}
+		System.out.print(a);
+
+	}
+
+}
